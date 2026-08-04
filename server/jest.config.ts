45b6b2c0db0b0cb275f68b30f47e5accef461d19
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-08-04 12:23:49 +0100

    build: scaffold server TS project

---
+++ server/jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globalSetup: "<rootDir>/src/test/setup.ts",
+  testMatch: ["**/*.test.ts"],
+  clearMocks: true,
+  coveragePathIgnorePatterns: ["/node_modules/", "/prisma/"],
+};
+export default config;
